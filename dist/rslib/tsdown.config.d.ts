5bf45845f6553a8dc6274b5763357cb08f86c04c
ocavue <ocavue@gmail.com>
2025-11-20 15:32:57 +1100

    update dependencies

--- dist/rslib/tsdown.config.d.ts
+++ dist/rslib/tsdown.config.d.ts
@@ -1,2 +1,2 @@
-declare const _default: import("tsdown").UserConfig & import("tsdown").UserConfigFn;
+declare const _default: import("tsdown").UserConfig & import("tsdown").UserConfig[] & (import("tsdown").UserConfigFn & import("tsdown").UserConfigExport);
 export default _default;
